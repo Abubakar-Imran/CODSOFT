3c093d8107fc262782a842ce8999ede59c9aaf33
Abubakar Imran <116654312+Abubakar-Imran@users.noreply.github.com>
2024-05-15 23:02:55 +0500

    Update Product.js

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -8,17 +8,17 @@ export default function Product(props) {
   const [cart,setCart] = useCart();
 
   return (
-    <div className='my-2' style={{boxShadow: 'rgba(0, 0, 0, 1) 0px 5px 15px', margin: '15px'}}>
-      <div className="card" style={{ maxHeight: '450px', maxWidth: '400px', textAlign: 'center' }}>
+    <div className='my-3' style={{boxShadow: 'rgba(0, 0, 0, 1) 0px 5px 15px'}}>
+      <div className="card" style={{ height: '500px', maxWidth: '400px', textAlign: 'center', paddingTop: '25px' }}>
         <img src={imageUrl ? imageUrl : "https://blog.logrocket.com/wp-content/uploads/2022/06/build-random-number-generator-blockchain-nocdn.png"} className="card-img-top" alt="..." width={'200px'} height={'200px'}/>
         <div className="card-body" style={{ backgroundColor: 'rgb(198 228 242)'}}>
           <hr />
           <h5 className="card-title">{name}</h5>
           <hr />
-          <p className="card-text">{description}</p>
-          <div className='container'>
+          <p className="card-text" style={{height: '35%'}}>{description}</p>
+          <div className='container' style={{justifyContent: 'center'}}>
             <p className="card-text" style={{fontWeight: 'bold'}}>Price: Rs.{price}
-            <button className='btn btn-info' style={{marginLeft: '25px'}}
+            <button className='btn btn-info' style={{marginLeft: '10px'}}
             onClick={() => {
               setCart([...cart,props])
               localStorage.setItem('cart', JSON.stringify([...cart, props]))
